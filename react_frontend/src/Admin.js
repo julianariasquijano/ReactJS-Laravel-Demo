c64d08ae528c82d15f7f96942023d08117c85f1f
Julian Arias <julian.arias@enjoyframework.com>
2019-07-24 16:49:18 -0500

    Implement app bar with panels

--- react_frontend/src/Admin.js
+++ react_frontend/src/Admin.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import './App.css';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 
-function Admin() {
+//Seting tabs basic functionality
+function TabContainer(props) {
   return (
-    <div>
-    Admin
-    </div>
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
   );
 }
 
-export default Admin;
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function Admin() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Hotels" />
+          <Tab label="Room Types" />
+        </Tabs>
+      </AppBar>
+      {value === 0 && <TabContainer>H Lists</TabContainer>}
+      {value === 1 && <TabContainer>RT List</TabContainer>}
+    </div>
+  );
+}
