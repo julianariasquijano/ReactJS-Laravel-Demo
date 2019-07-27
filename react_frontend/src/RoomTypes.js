e9c9b4d7b275879f7aa93bb89f1a18b80905d107
Julian Arias <julian.arias@enjoyframework.com>
2019-07-27 17:56:57 -0500

    ok crud for room types -all deletes are pending-

--- react_frontend/src/RoomTypes.js
+++ react_frontend/src/RoomTypes.js
@@ -1,71 +1,249 @@
 
 import React,{Component} from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
 
-function ObjectMap(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
 
-const rows = [
-    ObjectMap('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    ObjectMap('Ice cream sandwich', 237, 9.0, 37, 4.3),
-];
+import TextField from '@material-ui/core/TextField';
+
+import Config from './Config'
+
+
+
+let actualData={}
+let lastRowIdUpdated=0
+let validationMessages={}
 
 class RoomTypes extends Component {
 
     constructor(props){
         super()
         this.state = {
-            source:props.source,
+            connectionError:false,
+            loadingData:true,
+            rows:[],
+            detailsOpened:false,
+            validationMessages:{}
+            //validationMessages:JSON.parse(JSON.stringify(validationMessages))
+
         }
 
     }
 
-    render(){
+    componentWillMount() {
+        fetch(Config.api + '/room_types')
+          .then(response => response.json())
+          .then(jsonObject => {
+            //Asigning each element the position in array, in order to facilitate the automatic edition  
+            let rows = jsonObject.data
+            let tempRows = []
+            let elementCounter = 0;
+            rows.forEach(element => {
+                element.position = elementCounter
+                element.image=''
+                tempRows.push(element)
+                elementCounter++
+            });
+            this.setState({rows:tempRows,loadingData:false})
+          })
+          .catch(ex => this.setState({connectionError:true,loadingData:false}));
+      }    
 
-        let content=''
-        if (this.state.source === 'hotels'){
-            content = <h1>HOTELS</h1>
+    updateInputValue = function (event) {
+        event.persist()
+        actualData[event.target.name]= event.target.value
+    }
+    openDetails = () => {
+        actualData= {id:0}
+        this.setState({
+            detailsOpened:true,
+        })
+    }
+
+    openDetailsWithRow = (row) => {
+        actualData = JSON.parse(JSON.stringify(row))
+        this.setState({
+            detailsOpened:true,
+        })
+    }
+    closeDetails = () => {
+        this.resetValidationMessages()
+        this.setState({
+            detailsOpened:false,
+        })
+    }
+    resetValidationMessages = () => {
+        validationMessages = {
+            type:'',
         }
-        else if (this.state.source === 'room_types'){
-            
-            content = <div>ROOM TYPES</div>
+        this.setState({
+            validationMessages:validationMessages,
+        })
+    }
+    validateForm = () => {
+        let validationResult = true
+        this.resetValidationMessages()
+        if(actualData.type === undefined || actualData.type === null || actualData.type ===''){
+            validationResult = false;
+            validationMessages.type='Required'
         }
+        this.setState({validationMessages : validationMessages}) ;
+        return validationResult
+    }
+
+    saveRowRemote = () => {
+        if(!this.validateForm())return
+
+        let method='POST'
+        let url = Config.api + '/room_type'
+        if(actualData.id !==0){
+            method = 'PUT'
+            url += '/'+ actualData.id
+        } 
+
+        this.setState({loadingData:true})
+
+        url += '?' + Object.keys(actualData)
+          .map(key => `${key}=${actualData[key].toString()}`)
+          .join('&');
+
+        fetch(url, {method: method})
+            .then(response => response.json())
+            .then(jsonObject => {
+            //lastRowIdUpdated is used to correctly edit a recently created row
+            lastRowIdUpdated=jsonObject.data.id
+            this.setState({loadingData:false})})        
+            .then(response => this.saveRow())      
+    }
+
+    saveRow = () => {
+        let tempRows = JSON.parse(JSON.stringify(this.state.rows))
+        if(actualData.id ===0){
+            actualData.position = this.state.rows.length
+            actualData.id = lastRowIdUpdated
+            tempRows.push(actualData)
+        }
+        else {
+            tempRows[actualData.position] = JSON.parse(JSON.stringify(actualData))
+        }
+
+        this.setState({
+            detailsOpened:false,
+            rows:JSON.parse(JSON.stringify(tempRows))
+        })
+        this.closeDetails()
+    }
+
+
+    render(){
 
         return (
             <div>
-                {content}
+                { !this.state.connectionError && (
+                  <Fab 
+                  variant="round" 
+                  color='primary' 
+                  onClick={this.openDetails} 
+                  >
+                    <AddIcon />
+                </Fab>
+                )}
+                &nbsp;&nbsp;&nbsp;
+                <span style={{fontSize:'30px',fontWeight:'bold'}}>Room Types</span>
+                { this.state.loadingData && (
+                    <div>
+                        <br/>
+                        <CircularProgress></CircularProgress>
+                    </div>
+                )}
+                <br/>
+                <br/>
                 <Paper >
-            <Table >
-              <TableHead>
-                <TableRow>
-                  <TableCell>Dessert (100g serving)</TableCell>
-                  <TableCell align="right">Calories</TableCell>
-                  <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                  <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                  <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map(row => (
-                  <TableRow key={row.name}>
-                    <TableCell component="th" scope="row">
-                      {row.name}
-                    </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Paper>                
+                    <Table >
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Room Type</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.state.rows.map(row => (
+                            <TableRow key={row.position}>
+                                <TableCell component="th" scope="row">
+                                    {row.type}
+                                </TableCell>
+                                <TableCell >
+                                    <Fab id={row.id}
+                                        variant="round" 
+                                        color='primary' 
+                                        onClick={() => {this.openDetailsWithRow(row)}} 
+                                    >
+                                        <EditIcon/>
+                                    </Fab>
+                                </TableCell>
+                            </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Paper>
+
+                { this.state.connectionError && (
+                    <div>
+                        <Snackbar
+                            anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                            }}
+                            open={true}
+                            autoHideDuration={6000}
+                        >
+                        <SnackbarContent
+                            style={{backgroundColor:'darkred'}}
+                            message={
+                                <span>
+                                Connection Error
+                                </span>
+                            }
+
+                        />
+                        </Snackbar>
+                    </div>
+                )}
+                <Dialog open={this.state.detailsOpened} onClose={this.closeDetails}  >
+                    <DialogContent>
+                        <h3>Room Type Details</h3>
+                        <span className='controlWraperStyle'  >
+                            <Button variant="contained" color="primary" onClick={this.saveRowRemote}>
+                                Save
+                            </Button>
+                        </span>
+                        <span className='controlWraperStyle' >
+                            <Button variant="contained" color="secondary" onClick={this.closeDetails} >
+                                Cancel
+                            </Button>
+                        </span>                  
+                        <br/>
+
+                        <span className="controlWraperStyle" >
+                            <div className='errorMessages' >{this.state.validationMessages.type}</div>
+                            <TextField inputProps={{name:'type'}}  label="Room Type" variant="outlined" defaultValue={actualData.type} onChange={this.updateInputValue} />
+                        </span>
+
+                    </DialogContent>
+                </Dialog>          
             </div>
         );
     }
