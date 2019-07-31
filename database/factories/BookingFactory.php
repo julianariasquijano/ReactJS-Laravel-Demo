a25b3ca02f4c6b37c3a2ee580323b2f42c9ca09d
Julian Arias <julian.arias@enjoyframework.com>
2019-07-30 23:52:25 -0500

    avoid reverse dates

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -9,10 +9,12 @@ use Faker\Generator as Faker;
 $factory->define(Booking::class, function (Faker $faker) {
 
     $roomIds = Room::all()->pluck('id')->toArray();
+    $dateStart = $faker->dateTimeInInterval($date = '+1 week', $interval = '+3 days', $timezone = null);
+    $dateEnd = $faker->dateTimeInInterval($date = '+2 week', $interval = '+3 days', $timezone = null);
     return [
         'room_id' => $faker->randomElement($roomIds),
-        'date_start' => $faker->date,
-        'date_end' => $faker->date,
+        'date_start' => $dateStart,
+        'date_end' => $dateEnd,
         'customer_name' => $faker->name,
         'customer_email' => $faker->email,
         'total_nights' => $faker->numberBetween(3,7)
